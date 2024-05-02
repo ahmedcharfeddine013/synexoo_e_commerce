2611e6f151cc9dd10b3dd615d9949cef5c14bb2b
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-03 00:18:57 +0100

    carousel

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,6 +4,21 @@ import { product } from "@/types/product";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import Image from "next/image";
 
 export default function Hero() {
   const [carouselItems, setCarouselItems] = useState<product[] | null>();
@@ -18,7 +33,7 @@ export default function Hero() {
       }
     };
 
-    fetchCarouselItems()
+    fetchCarouselItems();
   });
 
   if (carouselItems == null)
@@ -28,7 +43,46 @@ export default function Hero() {
       </div>
     );
 
-  return <div></div>;
+  return (
+    <div className="flex items-center h-screen justify-center">
+      <CarouselItems products={carouselItems} />
+    </div>
+  );
 }
 
-
+export function CarouselItems({ products }: { products: product[] }) {
+  return (
+    <Carousel className="w-full">
+      <CarouselContent>
+        {products.map((product) => (
+          <CarouselItem
+            key={product.id}
+            className="grid grid-cols-2 place-items-center w-screen"
+          >
+            {/* Right side */}
+            <div className="p-1 flex items-center justify-center">
+              <Image
+                src={product.image}
+                alt={product.title}
+                width={400}
+                height={400}
+              ></Image>
+            </div>
+            {/* left side  */}
+            <Card>
+              <CardHeader>
+                <CardTitle>{product.title}</CardTitle>
+                <CardDescription>{product.description}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div>Price: ${product.price}</div>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
