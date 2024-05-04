074ef69a92d6f0708a055d8e40f19e3ea6497d8d
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-04 11:29:20 +0100

    pages structure fixed

--- src/app/(root)/[category]/layout.tsx
+++ src/app/(root)/[category]/layout.tsx
@@ -1,9 +1,17 @@
-import Filter from "@/components/Filter";
+import { Navbar } from "@/components/Navbar";
+
+import Logo from "@/components/Logo";
+import { ShoppingCart } from "lucide-react";
+import { Heart } from "lucide-react";
+import { Avatar } from "@/components/ui/avatar";
+import Nav from "@/components/Nav";
 import PageHeader from "@/components/PageHeader";
-import Products from "@/components/Products";
-import SearchProduct from "@/components/SearchProduct";
 import ShopByCategory from "@/components/ShopByCategory";
+import Filter from "@/components/Filter";
+import SearchProduct from "@/components/SearchProduct";
 import Sort from "@/components/Sort";
+
+export const dynamic = "force-dynamic";
 
 export default function Layout({
   children,
@@ -12,8 +20,6 @@ export default function Layout({
 }>) {
   return (
     <div className="flex items-center justify-center flex-col pt-24">
-      <PageHeader>SHOP</PageHeader>
-      <ShopByCategory />
       <div className="flex flex-col items-center justify-between w-full">
         <Filter />
         <div className="flex flex-col md:flex-row  items-center justify-center gap-10 w-full">
