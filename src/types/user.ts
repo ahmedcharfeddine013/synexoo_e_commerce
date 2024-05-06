7ef0a3c58272ac5fdd5916de9b61b311e7d475d2
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-06 03:02:55 +0100

    user types

---
+++ src/types/user.ts
@@ -0,0 +1,28 @@
+interface Geolocation {
+  lat: string;
+  long: string;
+}
+
+interface Address {
+  geolocation: Geolocation;
+  city: string;
+  street: string;
+  number: number;
+  zipcode: string;
+}
+
+interface Name {
+  firstname: string;
+  lastname: string;
+}
+
+interface User {
+  id: number;
+  email: string;
+  username: string;
+  password: string;
+  name: Name;
+  phone: string;
+  __v: number;
+  address: Address;
+}
