e96049c4c6bfd5ff7c5bb1e4da354ed9eb0c2c50
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-28 22:34:09 +0100

    carousel fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,7 @@
 "use client";
 
-import { product } from "@/types/product";
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Loading from "./Loading";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./ui/carousel";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "./ui/card";
 import Image from "next/image";
-import { Button } from "./ui/button";
-import Link from "next/link";
-import PageHeader from "./PageHeader";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -32,45 +11,17 @@ import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import { bannerImages } from "@/constants";
 
 export default function Hero() {
-  const [carouselItems, setCarouselItems] = useState<product[] | null>();
-
-  useEffect(() => {
-    const fetchCarouselItems = async () => {
-      try {
-        const res = await axios.get(
-          "https://fakestoreapi.com/products/category/men's%20clothing"
-        );
-        const data = res.data;
-        const sortedData = data.sort(
-          (a: product, b: product) => b.rating.rate - a.rating.rate
-        );
-        setCarouselItems(sortedData);
-      } catch (error) {
-        console.log("Error fetching carousel items :", error);
-      }
-    };
-
-    fetchCarouselItems();
-  });
-
-  if (carouselItems == null)
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <Loading />
-      </div>
-    );
-
   return (
-    <div className="flex flex-col items-center h-screen justify-center gap-20">
-      <PageHeader>TOP RATED</PageHeader>
-      <CarouselItems products={carouselItems} />
+    <div className="flex flex-col items-center w-full h-screen justify-center">
+      <CarouselItems />
     </div>
   );
 }
 
-function CarouselItems({ products }: { products: product[] }) {
+function CarouselItems() {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex: number) => {
@@ -85,16 +36,16 @@ function CarouselItems({ products }: { products: product[] }) {
           pagination={{ type: "bullets" }}
           modules={[Navigation, Pagination]}
           onSwiper={(swiper) => console.log(swiper)}
-          className="h-96 w-full rounded-lg"
+          className="h-[500px] w-full rounded-lg"
         >
-          {products.map((product, index) => (
+          {bannerImages.map((product, index) => (
             <SwiperSlide key={index}>
               <div className="flex h-full w-full items-center justify-center">
                 <Image
-                  src={product.image}
-                  alt={product.title}
-                  width={300}
-                  height={300}
+                  src={product}
+                  alt={index.toString()}
+                  width={2000}
+                  height={2000}
                   className="block h-full w-full object-cover"
                 />
               </div>
@@ -103,48 +54,5 @@ function CarouselItems({ products }: { products: product[] }) {
         </Swiper>
       </div>
     </section>
-    // <Carousel className="w-full">
-    //   <CarouselContent>
-    //     {products.map((product) => (
-    //       <CarouselItem
-    //         key={product.id}
-    //         className="grid grid-cols-1 md:grid-cols-2 place-items-center w-screen"
-    //       >
-    //         {/* Right side */}
-    //         <div className="p-1 flex items-center justify-center">
-    //           <Image
-    //             src={product.image}
-    //             alt={product.title}
-    //             width={300}
-    //             height={300}
-    //             className="h-[300px] w-[300px] object-contain"
-    //           ></Image>
-    //         </div>
-    //         {/* left side  */}
-    //         <Card className="border-none shadow-none">
-    //           <CardHeader className="flex flex-col gap-5">
-    //             <CardTitle>{product.title}</CardTitle>
-    //             <CardDescription>{product.description}</CardDescription>
-    //           </CardHeader>
-    //           <CardContent>
-    //             <div className="flex flex-col md:flex-row items-center justify-between gap-4">
-    //               {" "}
-    //               <p>
-    //                 Price:{" "}
-    //                 <span className="text-red-500">${product.price}</span>
-    //               </p>
-    //               <p>Rating : {product.rating.rate}</p>
-    //               <Button asChild>
-    //                 <Link href={`/product/${product.id}`}>EXPLORE ITEM</Link>
-    //               </Button>
-    //             </div>
-    //           </CardContent>
-    //         </Card>
-    //       </CarouselItem>
-    //     ))}
-    //   </CarouselContent>
-    //   <CarouselPrevious className="border-none left-0  " />
-    //   <CarouselNext className="border-none right-0" />
-    // </Carousel>
   );
 }
