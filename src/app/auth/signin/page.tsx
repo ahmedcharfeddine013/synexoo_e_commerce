257ca73c695cd2ba6b39b8af5abf5b2436827d8a
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-05-30 12:38:01 +0100

    init

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-import LoginForm from '@/components/LoginForm'
-import React from 'react'
+import LoginForm from "@/components/LoginForm";
+import React from "react";
 
 export default function SignInPage() {
   return (
-    <div className='flex items-center justify-center h-screen w-screen bg-primary'>
+    <div className="flex items-center justify-center h-screen w-screen bg-primary">
       <LoginForm />
     </div>
-  )
+  );
 }
