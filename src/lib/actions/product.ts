58d467361b5b12532e1224beb198b56f0af74753
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-07-15 00:05:05 +0100

    fetch all products function

--- src/lib/actions/product.ts
+++ src/lib/actions/product.ts
@@ -1,2 +1,21 @@
 import axios from "axios";
 
+export async function getProducts() {
+  try {
+    const res = await axios.get("https://api.escuelajs.co/api/v1/products");
+    const data = res.data();
+    return data;
+  } catch (error) {
+    console.log("Error fetching all products: ", error);
+  }
+}
+
+export async function getProductById(id: string) {
+  try {
+    const res = await axios.get("https://api.escuelajs.co/api/v1/products");
+    const data = res.data();
+    return data;
+  } catch (error) {
+    console.log(`Error fetching this products id ${id} data: `, error);
+  }
+}
