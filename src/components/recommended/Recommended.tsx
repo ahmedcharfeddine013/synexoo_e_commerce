cdfb0c6969567cf2f1169ba13181e8bee0aadb63
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-07-20 23:17:29 +0100

    recommended products fetching

--- src/components/recommended/Recommended.tsx
+++ src/components/recommended/Recommended.tsx
@@ -5,12 +5,21 @@ import React, { useEffect, useState } from "react";
 
 const Recommended = () => {
   const [products, setProducts] = useState<Product[]>();
+  const [recommendedProducts, setRecommendedProducts] = useState<Product[]>();
 
   useEffect(() => {
-    getProducts().then((data) => setProducts(data));
-  }, []);
+    getProducts()
+      .then((data) => setProducts(data))
+      .then(() => {
+        if (products) {
+          console.log(products);
+          const rp = products.slice(0, 3);
+          setRecommendedProducts(rp);
+        }
+      });
+  }, [products]);
 
-  return <section>{JSON.stringify(products)}</section>;
+  return <section>{JSON.stringify(recommendedProducts)}</section>;
 };
 
 export default Recommended;
