5941072c696da29cdbe8fb531a3162423328e1b1
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-07-24 23:42:50 +0100

    recommended products finished

--- src/components/recommended/Recommended.tsx
+++ src/components/recommended/Recommended.tsx
@@ -5,26 +5,10 @@ import React, { useEffect, useRef, useState } from "react";
 import ProductCard from "../product/ProductCard";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import { motion } from "framer-motion";
+import Loading from "../Loading";
 
 const Recommended = () => {
   const [products, setProducts] = useState<Product[]>();
-  const rowRef = useRef<HTMLDivElement>(null);
-  const [isMoved, setIsMoved] = useState(true);
-
-  const handleClick = (direction: string) => {
-    setIsMoved(true);
-
-    if (rowRef.current) {
-      const { scrollLeft, clientWidth } = rowRef.current;
-
-      const scrollTo =
-        direction === "left"
-          ? scrollLeft - clientWidth
-          : scrollLeft + clientWidth;
-
-      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
-    }
-  };
 
   useEffect(() => {
     getRecommendedProducts().then((data) => {
@@ -32,37 +16,19 @@ const Recommended = () => {
     });
   }, [products]);
 
-  if (products)
+  if (!products)
     return (
-      <motion.div
-        initial={{ opacity: 0, x: -200 }}
-        animate={{ opacity: 1, x: 0 }}
-        className=" space-y-0.5 md:space-y-2 py-5 pl-3"
-      >
-        <div className="relative group md:-md-2">
-          <ArrowLeft
-            className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
-              !isMoved && "hidden"
-            }`}
-            onClick={() => handleClick("left")}
-          />
-
-          <div
-            ref={rowRef}
-            className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
-          >
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-
-          <ArrowRight
-            className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
-            onClick={() => handleClick("right")}
-          />
-        </div>
-      </motion.div>
+      <div className="py-10">
+        <Loading />
+      </div>
     );
+  return (
+    <section className="flex-1 flex gap-4 my-10 md:flex-row flex-col ">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </section>
+  );
 };
 
 export default Recommended;
